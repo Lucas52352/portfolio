a7ef988793870a3c0e0946319cc94445b7435059
Lucas52352 <lucasrius86@gmail.com>
2023-09-13 18:35:10 -0300

    deploy config

--- vite.config.ts
+++ vite.config.ts
@@ -3,8 +3,8 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: "./porfolio",
   plugins: [react()],
-  base: "/portfolio/",
   server: {
     open: true,
     port: 3001
