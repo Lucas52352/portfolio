72913ab9353db1e5e3ddd0aec214be96b898b662
Lucas52352 <lucasrius86@gmail.com>
2023-09-13 18:53:44 -0300

    update

--- vite.config.ts
+++ vite.config.ts
@@ -3,10 +3,9 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/portfolio",
+  base: "/portfolio/",
   plugins: [react()],
   server: {
     open: true,
-    port: 3001
   }
 })
