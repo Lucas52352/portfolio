649b33ec0c024ad0bb27cf15c3be7230f7eaf985
Lucas52352 <lucasrius86@gmail.com>
2023-09-13 16:06:40 -0300

    contact me section

--- src/components/Contact/Contact.tsx
+++ src/components/Contact/Contact.tsx
@@ -1,10 +1,54 @@
+import { useRef } from 'react';
+import emailjs from '@emailjs/browser';
+import './Contact.css'
 
 const Contact = () => {
-  return (
-    <div>
-      <h1>Contact Me</h1>
-    </div>
-  )
-}
+  const form: any = useRef();
 
-export default Contact
+  const sendEmail = (event: any) => {
+    event.preventDefault();
+
+    emailjs
+      .sendForm(
+        'service_wtj0xe4',
+        'template_e3p7s9b',
+        form.current,
+        'grjzy8ufo72-jSoDH'
+      )
+      .then(
+        (result) => {
+          console.log(result.text);
+        },
+        (error) => {
+          console.log(error.text);
+        }
+      );
+  };
+
+  return (
+    <div className='maxContainer'>
+      <h1 className='contactTitle'>Contact Me</h1>
+      <div className='formContainer'>
+        <form ref={form} onSubmit={sendEmail}>
+          <div className='formItem'>
+            <label className='label'>Name</label>
+            <input type="text" name="user_name" />
+          </div>
+
+          <div className='formItem'>
+            <label className='label'>Email</label>
+            <input type="email" name="user_email" />
+          </div>
+
+          <div className='formItem'>
+            <label className='label'>Message</label>
+            <textarea className='textArea' name="message"/>
+          </div>
+            <input className='input' type="submit" value="Send" />
+        </form>
+      </div>
+    </div>
+  );
+};
+
+export default Contact;
--- src/components/Projects/Projects.tsx
+++ src/components/Projects/Projects.tsx
@@ -1,15 +1,62 @@
-import poke from '../../images/pokeapi.png'
-import wiseWallet from '../../images/wisewallet.jpeg'
+import poke from '../../images/pokeapi.png';
+import wiseWallet from '../../images/wisewallet.jpeg';
+import rym from '../../images/rickymorty.jpeg';
+import radtek from '../../images/radtek.jpeg';
+import './Projects.css';
 
 const Projects = () => {
   return (
-    <div>
+    <div className='maxContainer'>
       <h1>My Projects</h1>
 
-    <a href=''></a>
+      <div className="projectContainer1">
+        <div className="project1">
+          <img className="projectImg" src={radtek} alt="" />
+          <a
+            className="prLink"
+            href="https://github.com/Lucas52352/Gaming-eCommerce"
+            target="blank"
+          >
+            Repository
+          </a>
+        </div>
+        <div className="project1">
+          <img className="projectImg" src={poke} alt="" />
+          <a
+            className="prLink"
+            href="https://github.com/Lucas52352/PI-POKEMON"
+            target="blank"
+          >
+            Repository
+          </a>
+        </div>
+      </div>
 
+      <div className="projectContainer2">
+        <div className="project2">
+          <img className="projectImg" src={rym} alt="" />
+          <a
+            className="prLink"
+            href="https://github.com/Lucas52352/Rick_And_Morty"
+            target="blank"
+          >
+            Repository
+          </a>
+        </div>
+
+        <div className="project2">
+          <img className="projectImg" src={wiseWallet} alt="" />
+          <a
+            className="prLink"
+            href="https://github.com/Ezeko95/Wallet-Wise-client-app"
+            target="blank"
+          >
+            Repository
+          </a>
+        </div>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Projects
+export default Projects;
